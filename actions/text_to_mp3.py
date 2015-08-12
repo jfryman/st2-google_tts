9cee7d2379b3e224f30d68956fa6aacbd8e1f4e2
James Fryman <james@fryman.io>
2015-08-12 12:42:35 -0500

    fixing parameter

--- actions/text_to_mp3.py
+++ actions/text_to_mp3.py
@@ -5,8 +5,8 @@ from time import sleep
 
 
 class TextToMp3Action(Action):
-    def run(self, text, output_dir, lang='en', alert=None):
-        status, file_name = self.text2mp3(text, output_dir, lang, alert)
+    def run(self, text, output_dir, language='en', alert=None):
+        status, file_name = self.text2mp3(text, output_dir, language, alert)
         payload = { 'status': status, 'file_name': file_name }
         if ok:
             return payload
